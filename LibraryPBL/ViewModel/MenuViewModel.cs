35fe7a20b605e1ae85e5fde54168572887c64383
Eugen Vogel <vogeloygen@gmail.com>
2023-12-25 12:27:44 +0500

    исправление ошибки

--- LibraryPBL/ViewModel/MenuViewModel.cs
+++ LibraryPBL/ViewModel/MenuViewModel.cs
@@ -15,7 +15,6 @@ namespace LibraryPBL.ViewModel
         internal class MenuViewModel
         {
             #region переменые 
-            kkjkjkljkl;
             List<Book> allBooks = new();
             List<Book> selectedBooks = new();
             Book selectedBook;
